9106d551cf7e9977030d600c1fcc6823a9c60274
quedacoder <quedacoder@gmail.com>
2020-01-18 11:48:38 -0600

    setup the project

---
+++ src/main/java/com/quedacoder/securingwebapp/SecuringWebappApplication.java
@@ -0,0 +1,13 @@
+package com.quedacoder.securingwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuringWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuringWebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quedacoder/securingwebapp/SecuringWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quedacoder.securingwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuringWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
